950df8bdfaeef67a61c8d474af03a6ac8224564b
kurumi0512 <foreverlove0512t@gmail.com>
2025-04-23 16:55:47 +0800

    重新上傳新的資料夾位置

---
+++ src/case02/CoffeeTest.java
@@ -0,0 +1,11 @@
+package case02;
+
+public class CoffeeTest {
+
+	public static void main(String[] args) {
+		Coffee coffee = new Coffee(200.0, 100.0);
+		System.out.println(coffee.getCoffeeType());
+
+	}
+
+}
---
+++ src/case02/Guestbook.java
@@ -0,0 +1,28 @@
+package case02;
+
+import java.util.Date;
+
+public class Guestbook {
+
+	private String message;
+	private Date date;
+
+	public Guestbook(String message) {
+		this.message = message;
+		this.date = new Date(); // Date是java內建的
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	@Override
+	public String toString() {
+		return "Guestbook [message=" + message + ", date=" + date + "]";
+	}
+
+}
