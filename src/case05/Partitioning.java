f3b1f01dd495647d80bd11d1779f9e345861679f
kurumi0512 <foreverlove0512t@gmail.com>
2025-05-08 16:03:23 +0800

    250508 16:04

--- src/case05/Partitioning.java
+++ src/case05/Partitioning.java
@@ -5,6 +5,8 @@ import static java.util.stream.Collectors.partitioningBy;
 import java.util.List;
 
 //Partitioning是一切為二
+//這就是一個「List 裡面存放 Integer 泛型的例子」
+//表示 scores 是一個清單，裡面只能放 Integer 型別的資料，也就是整數。
 public class Partitioning {
 
 	public static void main(String[] args) {
@@ -12,8 +14,8 @@ public class Partitioning {
 		/*
 		 * "及格": [100, 90, 80] "不及格": [50, 40]
 		 */
+		// 使用Java Stream API 的 partitioningBy() 收集器來「分組」
 		System.out.println(scores.stream().collect(partitioningBy(score -> score >= 60)));
-
 	}
 
 }
